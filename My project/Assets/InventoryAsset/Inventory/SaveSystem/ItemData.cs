3a252a4506699dda30c9894824f2cd5cb23fdb17
tiffany <stiffany224@aol.com>
2025-02-20 18:50:11 -0500

    inventory, attempt 2
    i deleted the inventory branch and i'm trying again to create a new one. there are 700 changes and for that i am sorry.

---
+++ My project/Assets/InventoryAsset/Inventory/SaveSystem/ItemData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace InventorySystem
+{
+    
+    /// <summary>
+    /// Holds essential save information for items
+    /// </summary>
+    
+    [System.Serializable]
+    public class ItemData
+    {
+        public int amount;
+        public int position;
+        public string name;
+        public ItemData(int amount, string name, int position)
+        {
+            this.amount = amount;
+            this.name = name;
+            this.position = position;
+        }
+    }
+}
